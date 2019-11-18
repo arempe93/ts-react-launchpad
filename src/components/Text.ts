086ce95dbd674345d2795530d825ed53ecc343ed
Andrew Rempe <andrewrempe@gmail.com>
2019-11-18 13:43:10 -0600

    add icon story

--- src/components/Text.ts
+++ src/components/Text.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Text = styled.span`
-  color: ${p => p.theme[p.color]};
+  color: ${p => p.theme[p.color] || p.color};
 
   font-family: ${p => p.theme.font};
   font-size: ${p => p.size}rem;
---
+++ stories/components/Icon.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import ActionRow from '@/components/ActionRow'
+import Icon from '@/components/Icon'
+import Text from '@/components/Text'
+
+import Padding from '../Padding'
+
+storiesOf('components/Icon', module)
+  .add('default', () => (
+    <Padding>
+      <ActionRow gutter={3}>
+        <Text size={2}>
+          <Icon name='adjust' />
+        </Text>
+        <Text color='success500' size={2}>
+          <Icon name='check' />
+        </Text>
+        <Text color='warn500' size={2}>
+          <Icon name='ad' />
+        </Text>
+        <Text color='danger500' size={2}>
+          <Icon name='exclamation-triangle' />
+        </Text>
+        <Text color='#0052CC' size={2}>
+          <Icon name='atlassian' prefix='fab' />
+        </Text>
+        <Text color='grey500' size={2}>
+          <Icon name='closed-captioning' prefix='far' />
+        </Text>
+      </ActionRow>
+    </Padding>
+  ))
